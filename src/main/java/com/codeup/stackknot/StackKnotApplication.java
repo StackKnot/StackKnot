34dd27f6c83a778472e5c3495e686723632d642f
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2022-03-07 16:38:27 -0600

    first commit

---
+++ src/main/java/com/codeup/stackknot/StackKnotApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.stackknot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackKnotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackKnotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/stackknot/StackKnotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.stackknot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackKnotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
