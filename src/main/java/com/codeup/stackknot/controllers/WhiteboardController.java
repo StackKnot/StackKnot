81c3a424184689413edaac7e8607e2cf6de1cd14
gary_s <smithgd34@gmail.com>
2022-03-17 16:14:17 -0500

    completed backend random from whiteboardStart.html

--- src/main/java/com/codeup/stackknot/controllers/WhiteboardController.java
+++ src/main/java/com/codeup/stackknot/controllers/WhiteboardController.java
@@ -1,30 +1,13 @@
 package com.codeup.stackknot.controllers;
 
-import com.cloudinary.utils.ObjectUtils;
 import com.codeup.stackknot.models.Whiteboard;
 import com.codeup.stackknot.repositories.UserRepository;
 import com.codeup.stackknot.repositories.WhiteboardRepository;
-import org.cloudinary.json.JSONArray;
-import org.cloudinary.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import com.cloudinary.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.EntityNotFoundException;
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
 import java.util.*;
 
 @Controller
@@ -59,56 +42,33 @@ public class WhiteboardController {
         return "whiteboard/whiteboardStart";
     }
 
-    //    public Test generateTest(@PathVariable long setId) {
-//        List<Card>questions = cardDao.findAllBySetId(setId);
-//        List<String>answers = new List<String>() {
-//        };
-//        List<TestQuestion>testQuestions = new ;
-//
-//        for (Card question : questions) {
-//            answers.add(question.getAnswer());
-//        }
-//
-//        for (Card question : questions ) {
-//
-//            TestQuestion testQuestion = new TestQuestion();
-//
-//            testQuestion.setQuestion(question.getQuestion());
-//            testQuestion.setCorrectAnswer(question.getAnswer());
-//
-//            do {
-//                Collections.shuffle(answers);
-//                testQuestion.setWrongAnswer1(answers.get(1));
-//                testQuestion.setWrongAnswer2(answers.get(2));
-//                testQuestion.setWrongAnswer3(answers.get(3));
-//            } while ( testQuestion.getCorrectAnswer().equals(testQuestion.getWrongAnswer1()) || testQuestion.getCorrectAnswer().equals( testQuestion.getWrongAnswer2()) || testQuestion.getCorrectAnswer().equals(testQuestion.getWrongAnswer3())
-//            || testQuestion.getWrongAnswer1().equals(testQuestion.getWrongAnswer2()) || testQuestion.getWrongAnswer2().equals(testQuestion.getWrongAnswer3()) || testQuestion.getWrongAnswer1().equals(testQuestion.getWrongAnswer3()));
-//
-//
-//            testQuestions.add(testQuestion);
-//        }
-//
-//        return new Test(testQuestions);
-//    }
-    public Whiteboard shuffleBoards(@PathVariable Long id) {
-        List<Whiteboard> allBoardIds = whiteboardDao.findAllById(id);
-        List<Long> ids = new ArrayList<>();
-        List<Long> randomId = new ArrayList<>();
-        for (Whiteboard board : allBoardIds) {
-            ids.add(board.getId());
+    public Long shuffleBoards(List<Whiteboard> boards) {
+        boards = whiteboardDao.findAll();
+        Collections.shuffle(boards);
+        List<Long> getIds = new ArrayList<>();
+        for(Whiteboard board : boards) {
+            getIds.add(board.getId());
         }
-        Collections.shuffle(ids);
-        Whiteboard singleWhiteboard = new Whiteboard();
-        singleWhiteboard.setId(ids.get(0));
+        return getIds.get(0);
 
-        return singleWhiteboard;
+    }
 
+    @PostMapping("/whiteboardStart")
+    public String sendToRandomWhiteboard() {
+        long id = shuffleBoards(whiteboardDao.findAll());
+        return "redirect:whiteboard/" + id;
     }
 
     @GetMapping("/whiteboard/{id}")
     public String showEditor(@PathVariable long id, Model model) {
-        model.addAttribute("whiteboard", shuffleBoards(id));
+        model.addAttribute("whiteboard", whiteboardDao.getById(id));
         return "whiteboard/whiteboard";
+    }
+
+    @PostMapping("/whiteboard/{id}/next")
+    public String sendToNextWhiteboard() {
+        Long id = shuffleBoards(whiteboardDao.findAll());
+        return "redirect:whiteboard/" + id;
     }
 
     //     ADMIN UPLOAD ABILITY
