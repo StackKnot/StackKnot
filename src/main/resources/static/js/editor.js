0ba113ec8b755e0dcdbcb81200226a77686b8820
gary_s <smithgd34@gmail.com>
2022-03-21 14:19:46 -0500

    editor visible and base styling complete

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-    // CodeMirror.modeURL = "../../static/js/codemirror/mode/%N/%N.js"
     let code = $(".codemirror-textarea")[0];
     let editor = CodeMirror.fromTextArea(code,{
         lineNumbers : true,
         theme: "dracula",
         mode: "javascript"
     });
-    editor.setSize("600", "250");
+    editor.setSize("700", "450");
 })
