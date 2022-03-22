de63b37d2dd5cb0eacb5ca6238bfc63757677a32
Juan Longoria <juan.a.longoria66@gmail.com>
2022-03-22 10:06:36 -0500

    sendgrid email contact form template

--- src/main/resources/static/js/sendemail.js
+++ src/main/resources/static/js/sendemail.js
@@ -2,24 +2,27 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 // javascript
-
-const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey('') PLUG IN API KEY DIRECTLY TO SEND EMAIL WITH NODE JS, DELETE AFTER SENT
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+//Need Node.js Module to Run
 
 
-const msg = {
-    to: ['test@gmail.com','jlongoria66@yahoo.com'], // Change to your recipient
-    from: 'stackknot@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-    .send(msg)
-    .then(() => {
-        console.log('Email sent')
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+// require('dotenv').config
+// const sgMail = require('@sendgrid/mail')
+// // sgMail.setApiKey('') PLUG IN API KEY DIRECTLY TO SEND EMAIL WITH NODE JS, DELETE AFTER SENT
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+//
+//
+// const msg = {
+//     to: ['test@gmail.com','jlongoria66@yahoo.com'], // Change to your recipient
+//     from: 'stackknot@gmail.com', // Change to your verified sender
+//     subject: 'Sending with SendGrid is Fun',
+//     text: 'and easy to do anywhere, even with Node.js',
+//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//     .send(msg)
+//     .then(() => {
+//         console.log('Email sent')
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
