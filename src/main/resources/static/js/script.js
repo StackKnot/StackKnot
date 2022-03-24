fa1b3088711487a93f276c82ce1468c8838d42ba
Juan Longoria <juan.a.longoria66@gmail.com>
2022-03-24 09:01:17 -0500

    worked on darkmode and static images styling

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,12 @@
+//script.js
+
+function myFunction() {
+    const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+    localStorage.setItem('darkmode', !wasDarkmode);
+    const element = document.body;
+    element.classList.toggle('dark-mode', !wasDarkmode);
+}
+
+function onload() {
+    document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+}
